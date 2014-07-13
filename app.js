64abba07bab7a5f2d4125899c317363324c83fee
Worawut <tonmanna@gmail.com>
2014-07-14 06:20:41 +0700

    Message

--- app.js
+++ app.js
@@ -7,23 +7,25 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var login = require('./routes/backend/login');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('layout', 'layout');
 
+app.use(expressLayouts);
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(expressLayouts);
+
 
 app.use('/', routes);
+app.use('/login', login);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
--- routes/backend/login.js
+++ routes/backend/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'CMS 3.0' ,layout: 'partial/layout'});
+    res.render('backend/login', { title: 'CMS 3.0' ,layout: 'partial/backend'});
 });
 
 module.exports = router;
