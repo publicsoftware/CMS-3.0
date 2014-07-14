45089ae7669fb141c2bde1b8f87764bab8425996
Worawut <tonmanna@gmail.com>
2014-07-14 07:25:36 +0700

    Fix NotFound

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('layout', 'partial/layout'); // Default layout partial
+
 
 app.use(expressLayouts);
 app.use(favicon());
